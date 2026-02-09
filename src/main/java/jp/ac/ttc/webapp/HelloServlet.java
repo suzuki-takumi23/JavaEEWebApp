e08b794e844f69a6545a3bdf3206654066c71b80
H24304013\koyama <ts24304013@ga.ttc.ac.jp>
2026-02-09 14:42:35 +0900

    first commit

---
+++ src/main/java/jp/ac/ttc/webapp/HelloServlet.java
@@ -0,0 +1,18 @@
+package jp.ac.ttc.webapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+ @Override
+protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	 resp.setContentType("text/html charset=UTF-8");
+	 PrintWriter out = resp.getWriter();
+	 out.println("<h1>Hello, world</h1>");
+ }
+}
